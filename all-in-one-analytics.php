ca188a37ae7399a952cef96788b2a2ab4962477f
Juan G <hello@juangonzalez.com.au>
2019-07-10 00:58:25 +1000

    updated READMEs

--- all-in-one-analytics.php
+++ all-in-one-analytics.php
@@ -1,12 +1,7 @@
 <?php
 
 /**
- * This is the plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
+ * All In One Analytics :)
  *
  * @link              https://in8.io
  * @since             1.0.0
@@ -40,9 +35,6 @@ define( 'PLUGIN_NAME_PLUGIN_NAME', 'all-in-one-analytics' );
 define( 'PLUGIN_NAME_BASE_DIR', plugin_dir_path( __FILE__ ) );
 
 /**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
  */
 define( 'ALL_IN_ONE_ANALYTICS_VERSION', '1.0.0' );
 
@@ -75,10 +67,6 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-all-in-one-analytics.php';
 
 /**
  * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
  *
  * @since    1.0.0
  */
